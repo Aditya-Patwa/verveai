31410d560302c052a9cd16bbd4dbcc83e2dab503
Aditya Patwa <adityapatwa.tech@gmail.com>
2024-07-04 16:11:28 +0000

    Some Error Fixed

--- components/custom/Header.tsx
+++ components/custom/Header.tsx
@@ -51,8 +51,8 @@ export default function Header() {
                     Login
                 </a>
 
-                <Sheet className="grid md:hidden">
-                    <SheetTrigger className="px-3">
+                <Sheet>
+                    <SheetTrigger className="grid md:hidden px-3">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="white" className="size-8">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 9h16.5m-16.5 6.75h16.5" />
                         </svg>
